f09ba0d2704c455ae9559482c88fb2454ba291f5
coolbriggs123 <47996230+coolbriggs123@users.noreply.github.com>
2025-03-31 06:04:52 -0400

    Add files via upload

---
+++ Core.cs
@@ -0,0 +1,67 @@
+﻿
+using MelonLoader;
+using UnityEngine;
+using Il2CppScheduleOne.Employees;
+using System.IO;
+using Il2CppSystem.Collections.Generic;
+using Il2CppScheduleOne.Management;
+using static Il2CppVLB.Consts;
+
+[assembly: MelonInfo(typeof(ChemistEnhanced.Core), "ChemistEnhanced", "1.0.0", "Coolbriggs", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace ChemistEnhanced
+{
+
+
+    public class Core : MelonMod
+    {
+        private Chemist chemistComponent;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName != "Main")
+                return;
+
+            MelonCoroutines.Start(DelayedInit());
+        }
+
+        private System.Collections.IEnumerator DelayedInit()
+        {
+            yield return new WaitForSeconds(2f);  // Wait for 2 seconds
+
+            var chemistObject = GameObject.Find("Chemist(Clone)");
+            if (chemistObject == null)
+            {
+                LoggerInstance.Error("Could not find Chemist object");
+                yield break;
+            }
+
+            chemistComponent = chemistObject.GetComponent<Chemist>();
+            if (chemistComponent == null)
+            {
+                LoggerInstance.Error("Could not find Chemist component");
+                yield break;
+            }
+
+            MaxItems();
+        }
+
+        private void MaxItems()
+        {
+            if (chemistComponent == null || chemistComponent._configuration_k__BackingField == null) 
+                return;
+
+            var stations = chemistComponent._configuration_k__BackingField.Stations;
+            stations.MaxItems = 20;
+            LoggerInstance.Msg($"Max Items Patched");
+            LoggerInstance.Msg($"If you need any help join https://discord.gg/PCawAVnhMH");
+            LoggerInstance.Msg($"Happy Selling!");
+        }
+
+        public override void OnPreferencesSaved()
+        {
+            MaxItems();
+        }
+    }
+}
